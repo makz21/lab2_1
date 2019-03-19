0769819d20a9167103fa7e60ff2104c6196bc580
Maksymilian Reguła <maksymilian.regula@gmail.com>
2019-03-19 19:25:48 +0100

    testy_v1 added elementIsNotInSeq

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -10,6 +10,7 @@ public class BinarySearchTest {
 //    int [] tab = {1,2,3,4,5,6};
 //    int key = 1;
     public static final int ELEMENT = 2;
+    public static final int OTHER_ELEMENT = 3;
 
     @Test
     public void search() {
@@ -20,4 +21,10 @@ public class BinarySearchTest {
         int [] sequence = {ELEMENT};
         Assert.assertTrue(BinarySearch.search(ELEMENT,sequence).isFound());
     }
+    @Test
+    public void elementIsNotInSeq(){
+        int [] sequence = {ELEMENT};
+        Assert.assertFalse(BinarySearch.search(OTHER_ELEMENT,sequence).isFound());
+    }
+
 }
