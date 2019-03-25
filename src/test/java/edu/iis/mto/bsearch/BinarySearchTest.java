4ba4d10523c0bf3c19cc431f3df6deca74f7c86a
Maksymilian Reguła <maksymilian.regula@gmail.com>
2019-03-25 23:19:50 +0100

    testy_v1 added checking if elem is not multi

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -41,4 +41,11 @@ public class BinarySearchTest {
 
         Assert.assertEquals((sequence.length/2) +1, BinarySearch.search(ELEMENT, sequence).getPosition());
     }
+
+    @Test
+    public void elementNotInMultiSeq() {
+        int[] sequence = {ELEMENT + 1, ELEMENT + 2, ELEMENT + 3};
+
+        Assert.assertFalse(BinarySearch.search(ELEMENT, sequence).isFound());
+    }
 }
