9954a398b9181dc853c17719e73a0b64e9d37c1a
Maksymilian Reguła <maksymilian.regula@gmail.com>
2019-03-25 22:24:45 +0100

    Revert "testy_v1 added elementIsNotInSeq"
    This reverts commit 0769819d

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -2,13 +2,14 @@ package edu.iis.mto.bsearch;
 
 import org.junit.Assert;
 import org.junit.Test;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.is;
+
+import static org.junit.Assert.*;
 
 public class BinarySearchTest {
 
+//    int [] tab = {1,2,3,4,5,6};
+//    int key = 1;
     public static final int ELEMENT = 2;
-    public static final int OTHER_ELEMENT = 3;
 
     @Test
     public void search() {
@@ -19,59 +20,4 @@ public class BinarySearchTest {
         int [] sequence = {ELEMENT};
         Assert.assertTrue(BinarySearch.search(ELEMENT,sequence).isFound());
     }
-    @Test
-    public void elementIsNotInSeq(){
-        int [] sequence = {ELEMENT};
-        Assert.assertFalse(BinarySearch.search(OTHER_ELEMENT,sequence).isFound());
-    }
-
-    @Test
-    public void elementFirstInSeq() {
-        int[] sequence = {ELEMENT, ELEMENT + 1, ELEMENT + 2, ELEMENT + 3};
-
-        Assert.assertThat(ELEMENT, is(equalTo(sequence[BinarySearch.search(ELEMENT, sequence).getPosition()])));
-    }
-
-    @Test
-    public void elementLastInSeq() {
-        int[] sequence = {ELEMENT - 3, ELEMENT - 2, ELEMENT - 1, ELEMENT};
-
-        Assert.assertThat(ELEMENT, is(equalTo(sequence[BinarySearch.search(ELEMENT, sequence).getPosition()])));
-    }
-
-    @Test public void elementMidInSeq() {
-        int[] sequence = {ELEMENT - 2, ELEMENT - 1, ELEMENT, ELEMENT + 1, ELEMENT + 2};
-
-        Assert.assertThat(ELEMENT, is(equalTo(sequence[BinarySearch.search(ELEMENT, sequence).getPosition()])));
-    }
-
-    @Test
-    public void elementIsNotInMultiSeq() {
-        int[] sequence = {ELEMENT + 1, ELEMENT + 2, ELEMENT + 3};
-
-        Assert.assertThat(-1, is(equalTo(BinarySearch.search(ELEMENT, sequence).getPosition())));
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void searchElementInEmptySeq() {
-        int[] sequence = {};
-
-        BinarySearch.search(ELEMENT, sequence);
-    }
-
-    @Test
-    public void searchNegativeElementInSeq() {
-        int[] sequence = {ELEMENT - 4, ELEMENT - 3, ELEMENT - 2, ELEMENT - 1, ELEMENT, ELEMENT + 1, ELEMENT + 2};
-        int elementToFind = ELEMENT - 4;
-
-        Assert.assertThat(elementToFind, is(equalTo(sequence[BinarySearch.search(elementToFind, sequence).getPosition()])));
-    }
-
-    @Test
-    public void searchElementInSeqContainsSameElements() {
-        int[] sequence = {ELEMENT, ELEMENT, ELEMENT, ELEMENT, ELEMENT, ELEMENT, ELEMENT, ELEMENT};
-
-        Assert.assertThat((sequence.length - 1) / 2, is(equalTo(BinarySearch.search(ELEMENT, sequence).getPosition())));
-    }
-
 }
