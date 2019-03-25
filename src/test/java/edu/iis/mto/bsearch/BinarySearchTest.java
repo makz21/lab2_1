7c7d3681f77a794787d815967c67f652531cbb50
Maksymilian Reguła <maksymilian.regula@gmail.com>
2019-03-25 21:30:55 +0100

    binary search test first last in seq

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -2,13 +2,11 @@ package edu.iis.mto.bsearch;
 
 import org.junit.Assert;
 import org.junit.Test;
-
-import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
 
 public class BinarySearchTest {
 
-//    int [] tab = {1,2,3,4,5,6};
-//    int key = 1;
     public static final int ELEMENT = 2;
     public static final int OTHER_ELEMENT = 3;
 
@@ -27,4 +25,17 @@ public class BinarySearchTest {
         Assert.assertFalse(BinarySearch.search(OTHER_ELEMENT,sequence).isFound());
     }
 
+    @Test
+    public void elementFirstInSeq() {
+        int[] sequence = {ELEMENT, ELEMENT + 1, ELEMENT + 2, ELEMENT + 3};
+
+        Assert.assertThat(ELEMENT, is(equalTo(sequence[BinarySearch.search(ELEMENT, sequence).getPosition()])));
+    }
+
+    @Test
+    public void elementLastInSeq() {
+        int[] sequence = {ELEMENT - 3, ELEMENT - 2, ELEMENT - 1, ELEMENT};
+
+        Assert.assertThat(ELEMENT, is(equalTo(sequence[BinarySearch.search(ELEMENT, sequence).getPosition()])));
+    }
 }
