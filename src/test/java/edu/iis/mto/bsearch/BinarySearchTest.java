6d50e1defc5bf63d1e4cd73d15ba2b03e37f78c3
Maksymilian Reguła <maksymilian.regula@gmail.com>
2019-03-25 23:17:51 +0100

    testy_v1 added  checking if elem is on last position

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -7,30 +7,32 @@ import static org.junit.Assert.*;
 
 public class BinarySearchTest {
 
-
     public static final int ELEMENT = 2;
 
-    @Test
-    public void search() {
+    @Test public void search() {
     }
 
-    @Test
-    public void elementIsInSeq(){
-        int [] sequence = {ELEMENT};
-        Assert.assertTrue(BinarySearch.search(ELEMENT,sequence).isFound());
+    @Test public void elementIsInSeq() {
+        int[] sequence = {ELEMENT};
+        Assert.assertTrue(BinarySearch.search(ELEMENT, sequence).isFound());
     }
 
-    @Test
-    public void elementNotInSingleSeq(){
-        int[] sequence = {ELEMENT+1};
+    @Test public void elementNotInSingleSeq() {
+        int[] sequence = {ELEMENT + 1};
 
         Assert.assertFalse(BinarySearch.search(ELEMENT, sequence).isFound());
     }
 
-    @Test
-    public void elementFirstInSeq(){
-        int[] sequence = {ELEMENT, ELEMENT+1,ELEMENT+2,ELEMENT+3};
+    @Test public void elementFirstInSeq() {
+        int[] sequence = {ELEMENT, ELEMENT + 1, ELEMENT + 2, ELEMENT + 3};
 
-        Assert.assertEquals(0,BinarySearch.search(ELEMENT,sequence).getPosition());
+        Assert.assertEquals(0, BinarySearch.search(ELEMENT, sequence).getPosition());
     }
+
+    @Test public void elementLastInSeqTest() {
+        int[] sequence = {ELEMENT - 3, ELEMENT - 2, ELEMENT - 1, ELEMENT};
+
+        Assert.assertEquals(sequence.length, BinarySearch.search(ELEMENT, sequence).getPosition());
+    }
+
 }
