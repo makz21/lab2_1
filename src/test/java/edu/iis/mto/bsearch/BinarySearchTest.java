1129958fd7a62bc079651a8c3dd153e5e29a22c7
Maksymilian Reguła <maksymilian.regula@gmail.com>
2019-03-25 21:41:54 +0100

    binary search all test passed

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -51,4 +51,27 @@ public class BinarySearchTest {
 
         Assert.assertThat(-1, is(equalTo(BinarySearch.search(ELEMENT, sequence).getPosition())));
     }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void searchElementInEmptySeq() {
+        int[] sequence = {};
+
+        BinarySearch.search(ELEMENT, sequence);
+    }
+
+    @Test
+    public void searchNegativeElementInSeq() {
+        int[] sequence = {ELEMENT - 4, ELEMENT - 3, ELEMENT - 2, ELEMENT - 1, ELEMENT, ELEMENT + 1, ELEMENT + 2};
+        int elementToFind = ELEMENT - 4;
+
+        Assert.assertThat(elementToFind, is(equalTo(sequence[BinarySearch.search(elementToFind, sequence).getPosition()])));
+    }
+
+    @Test
+    public void searchElementInSeqContainsSameElements() {
+        int[] sequence = {ELEMENT, ELEMENT, ELEMENT, ELEMENT, ELEMENT, ELEMENT, ELEMENT, ELEMENT};
+
+        Assert.assertThat((sequence.length - 1) / 2, is(equalTo(BinarySearch.search(ELEMENT, sequence).getPosition())));
+    }
+
 }
